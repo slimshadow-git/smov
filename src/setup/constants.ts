f07a95ac34bf9b5cc18b72ade246dc534cea6463
slimshadow git <130247009+slimshadow-git@users.noreply.github.com>
2024-12-26 00:15:56 +0530

    Update constants.ts

--- src/setup/constants.ts
+++ src/setup/constants.ts
@@ -1,6 +1,6 @@
 export const APP_VERSION = import.meta.env.PACKAGE_VERSION;
 export const DISCORD_LINK = "discord.com";
-export const GITHUB_LINK = "https://github.com/slimshadow-git/movie";
+export const GITHUB_LINK = "https://github.com/slimshadow-git";
 export const TWITTER_LINK = "https://twitter.com/sameerbanchhor";
 export const GA_ID = import.meta.env.VITE_GA_ID;
 export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
